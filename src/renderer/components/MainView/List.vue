cb70c88310d5cd75fbf6344cc1baae6048d278ac
risou <risou.f@gmail.com>
2018-03-14 07:20:13 +0900

    Update List html

--- src/renderer/components/MainView/List.vue
+++ src/renderer/components/MainView/List.vue
@@ -14,13 +14,16 @@
         <div class="list_main">
           <h1 class="list_title">
             <a @click="selectArticle({ article: article })">{{ article.title }}</a>
+            <span class="group" v-if="article.group">{{ article.group.name }}</span>
           </h1>
           <div class="list_tags">
-            <div class="list_tag">
-              <a href="/tags"></a>
+            <div class="list_tag" v-for="tag in article.tags">
+              <span class="tag_name">{{ tag.name }}</span>
             </div>
           </div>
           <div class="list_stats">
+            <i class="fas fa-comments"> {{ article.comments_count }}</i>
+            <i class="far fa-smile"> {{ article.reactions_count }}</i>
           </div>
         </div>
       </div>
